6afeae23ba0cf696204c3c3186262213fe29dd4d
Nisarg Patel <np96380@gmail.com>
2022-10-29 00:18:18 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "../components/Sidebar";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        <Sidebar />
+        {/* <Center /> */}
+      </main>
+      <div>{/*<Player />*/}</div>
+    </div>
+  );
+};
+
+export default Home;
