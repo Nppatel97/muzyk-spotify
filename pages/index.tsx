e2b3ea091819cd47fa195dd5f6a97d59131951bb
Nisarg Patel <np96380@gmail.com>
2022-10-31 01:31:33 +0530

    fetching session before render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { getSession } from "next-auth/react";
 import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 
@@ -17,3 +16,13 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
